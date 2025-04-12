bd9edfd9ba1a376404c9165b1c2804a0e77e663a
Ahmad Bourhan <ahmadmghbourhan@gmail.com>
2025-04-13 00:19:53 +0100

    Add intentionally vulnerable SQL injection login to insecure version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     email = params[:email]
     password = params[:password]
-    user = User.find_by_sql("SELECT * FROM users WHERE email = '#{email}' AND password = '#{password}'").first
-
-
+  
+    # Vulnerable version: password is ignored in SQL logic
+    query = "SELECT * FROM users WHERE email = '#{email}' OR '1'='1'"
+    user = User.find_by_sql(query).first
+  
     if user
       session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in successfully"
@@ -16,6 +18,7 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+  
 
   def destroy
     session[:user_id] = nil
