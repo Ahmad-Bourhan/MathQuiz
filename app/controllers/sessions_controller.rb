fd42b7241baa62c30a9db48a7066f2263ecd0da4
Ahmad Bourhan <ahmadmghbourhan@gmail.com>
2025-04-13 13:43:02 +0100

    Fixed login issue and added working seed user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
+      puts "✅ Logged in: #{user.email}, session[:user_id] = #{session[:user_id]}"
       redirect_to quizzes_path, notice: "Logged in successfully!"
     else
+      puts "❌ Failed login attempt for email: #{params[:email]}"
       flash.now[:alert] = "Invalid email or password"
       render :new
     end
   end
+  
   
 
   def destroy
